ee950c446be1a3ed1ea69640e9fd4643f3a127ba
Enieber Cunha <eniebercunha@gmail.com>
2016-02-19 10:56:13 -0300

    proposta de separação de diretivas

--- inputLabel/main.js
+++ inputLabel/main.js
@@ -1,23 +1,3 @@
-/**
- * @license Todos os direitos reservados a JMJ.
- * @author GilbertoLeão <gilbertorleao@gmail.com>
- * @ngdoc overview
- * @name jmjInputLabel
- * @description
- * Diretiva resonsavél por criar um elemento label e um input na tela recebe os parametros:
-
- *  <b>label:</b> Exibe o label com o valor passado pelo componente.
- *
- *  <b>model:</b> Passa o model que onde o valor será selecionado.
- *
- *  <b>parametros html:</b> id, maxlength, minlength, placeholder
- *
- * <b>Exemplo:</b><br/><br/>
- *  <br/><label>Label</label>
- *  <input type="text" name="radio" value="{{radio.value}}" ng-model="$parent.model" aria-describedby="enum checkbox"></input>
- **/
-
-
 define(['text!extensions/diretivas/inputLabel/view.html'], function (view) {
 
     'use strict';
