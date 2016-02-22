94d81bfc4a12230568de1b2fd68e9179f1616d17
Enieber Cunha <eniebercunha@gmail.com>
2016-02-22 17:04:50 -0300

    add coverage

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,17 @@ module.exports = function(config) {
             'node_modules/angular-sanitize/angular-sanitize.js',
             'dist/jmj-diretivas.js',
             'src/**/*spec.js'
-        ]
+        ],
+        reporters: ['progress', 'coverage'],
+        preprocessors: {
+          // source files, that you wanna generate coverage for
+          // do not include tests or libraries
+          // (these files will be instrumented by Istanbul)
+          'src/**/*.js': ['coverage']
+        },
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/'
+        }
     });
 };
