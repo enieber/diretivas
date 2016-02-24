c1363b0d87659baaf358736e5109556ff307b170
Enieber Cunha <eniebercunha@gmail.com>
2016-02-24 17:17:05 -0300

    refectory label

--- src/inputLabelHorizontal/main-spec.js
+++ src/inputLabelHorizontal/main-spec.js
@@ -28,5 +28,14 @@ describe('Teste inputLabelHorizontal', function () {
         var input = getCompiledElement('<jmj-input-label-horizontal label="3 - Nº Guia Referenciada" required="true"></jmj-input-label-horizontal>', $scope);
         expect(input.find('input').hasClass('ng-invalid')).toEqual(true);
     });
+    it('deve conter o label passado por parametro', function () {
+        var label = getCompiledElement('<jmj-input-label-horizontal id="test" label="Teste" required="true"></jmj-input-label-horizontal>', $scope);
+        expect(label.find('label').text()).toEqual("Teste*:");
+    });
 
+    it('deve ficar sem o asterisco quando não for required', function(){
+        var label = getCompiledElement('<jmj-input-label-horizontal id="test"  label="Teste"></<jmj-input-label-horizontal >', $scope);
+        console.log(label);
+        expect(label.find('label').text()).toEqual("Teste:");
+    });
 });
--- src/label/main.js
+++ src/label/main.js
@@ -9,6 +9,19 @@ app.directive('jmjLabel', function() {
                 placeholder: '@',
                 model : '=',
                 disabled : '@'
+            },
+            link: function (scope, element, attr) {
+                scope.$on('submit', function(scope, form) {
+                    if (attr.id) {
+                        if (form[attr.id].$invalid ) {
+                            scope.currentScope.$broadcast('jmjValidateEvent', form[attr.id].$error);
+                            scope.currentScope.requerido = 'required';
+                            return;
+                        }
+                    }
+                    scope.currentScope.requerido = '';
+                });
+
             }
         };
     });
