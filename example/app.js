e0fb8ecc46f2d0d37050b72b5dc0f2391b53423a
Enieber Cunha <eniebercunha@gmail.com>
2016-03-04 17:00:11 -0400

    add disable in dateInput

--- example/app.js
+++ example/app.js
@@ -14,6 +14,57 @@ angular.module('app', ['jmj.diretivas', 'ngAnimate', 'ui.bootstrap', 'ui.mask'])
     })
     .controller('controller',function ($scope, jmjFormConfig) {
       $scope.model = {};
+      $scope.data = 2015-10-22;
+      $scope.codSeqRef = [
+          {
+            "id":1,
+            "quantidadeSolicitada":3,
+            "quantidadeAutorizada":2,
+            "valorSolicitado":5000,
+            "valorAutorizado":4800,
+            "sequencia":1,
+            "descricaoProcedimento":"Raios-X",
+            "solicitacao":null,
+            "servico":
+              {
+                "id":1,
+                "codigo":44,
+                "descricao":"Raios-X",
+                "registroAnvisa":null,
+                "codigoRefFabricante":null,
+                "autorizacaoFuncionamento":null,
+                "diretrizUtilizacao":null,
+                "codigoDespesa":null,
+                "unidadeMedida":null,
+                "tuss":65,
+                "classeServico":null
+              }
+          },
+          {
+              "id":2,
+              "quantidadeSolicitada":3,
+              "quantidadeAutorizada":2,
+              "valorSolicitado":5000,
+              "valorAutorizado":4800,
+              "sequencia":2,
+              "descricaoProcedimento":"Raios-X 2",
+              "solicitacao":null,
+              "servico":
+                {
+                  "id":1,
+                  "codigo":44,
+                  "descricao":"Raios-X 2",
+                  "registroAnvisa":null,
+                  "codigoRefFabricante":null,
+                  "autorizacaoFuncionamento":null,
+                  "diretrizUtilizacao":null,
+                  "codigoDespesa":null,
+                  "unidadeMedida":null,
+                  "tuss":65,
+                  "classeServico":null
+                }
+            }
+        ];
       $scope.submit = function () {
         toastr.success('Sucesso velho', 'OK!');
       };
